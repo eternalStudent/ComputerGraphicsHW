17f5dc84963a14a04c11ba4985f61e292d997d53
eternalStudent <noam.weisberg@gmail.com>
2015-04-19 04:48:38 +0300

    another camera constructor cosmetic change

--- RayTracing/Camera.java
+++ RayTracing/Camera.java
@@ -3,7 +3,7 @@ package RayTracing;
 public class Camera {
 	final Vector position;
 	
-	private final Vector screenDirection;
+	private final Vector screenNormal;
 	private final double screenHeight;
 
 	private final int imageHeight;
@@ -19,24 +19,21 @@ public class Camera {
 		double screenDistance, double screenWidth,
 		int imageWidth, int imageHeight) {
 
-		this.position = new Vector(px, py, pz);
-		Vector lookAt = new Vector(lx, ly, lz);
-
-		screenHeight = screenWidth*(imageHeight / imageWidth);
-
+		this.screenHeight = screenWidth*(imageHeight / imageWidth);
 		this.imageHeight = imageHeight;
 		this.imageWidth = imageWidth;
+		this.position = new Vector(px, py, pz);
+		
+		//constructing screenNormal & screenPlane
+		Vector lookAt = new Vector(lx, ly, lz);
+		screenNormal = lookAt.subtract(position);
+		Vector screenCenter = position.add(screenNormal.toLength(screenDistance));
+		Plane screenPlane = screenNormal.getPerpendicularPlaneAtPoint(screenCenter);
 
-		screenDirection = lookAt.subtract(position);
-		Vector screenCenter = position.add(screenDirection.toLength(screenDistance));
-
-		// This is the plane that the direction vector is perpendicular to.
-		Plane screenPlane = screenDirection.getPerpendicularPlaneAtPoint(screenCenter);
-
+		//constructing axes
 		Vector up = new Vector(ux, -uy, uz);
 		up = up.projectOntoPlane(screenPlane);
-
-		xAxis = screenDirection.cross(up).toLength(screenWidth / 2);
+		xAxis = screenNormal.cross(up).toLength(screenWidth / 2);
 		yAxis = up.toLength(screenHeight / 2);
 	}
 
@@ -45,7 +42,7 @@ public class Camera {
 		double alpha = (2*x - imageWidth) / (double) imageWidth;
 		double beta = (2*y - imageHeight) / (double) imageHeight;
 
-		Vector vectOfPixel = Vector.sum(screenDirection.normalize(),
+		Vector vectOfPixel = Vector.sum(screenNormal.normalize(),
 			xAxis.scale(alpha), yAxis.scale(beta));
 
 		return new Ray(position, vectOfPixel);
